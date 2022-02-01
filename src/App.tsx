1a1b249134dfca51d022ab6139952d3466b07f38
Harish Karthik <hhkk0010@outlook.com>
2022-02-01 23:13:57 +0530

    add svg react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Image from "./assets/logo.svg";
+import Image from "jsx:./assets/logo.svg";
+import svgUrl from "./assets/logo.svg";
+import "./App.css"
 
-export function App() {
-  return <Image />;
-}
+export const App = () => <img src={svgUrl} width={200}/>
