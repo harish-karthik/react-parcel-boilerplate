e6027d4ab26249cdeb478137f76246a201f313c2
Harish Karthik <hhkk0010@outlook.com>
2022-02-10 20:46:49 +0530

    add support for custom file structure with parcel-namer-rewrite

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React from "react";
 import Image from "jsx:./assets/logo.svg";
 import svgUrl from "./assets/logo.svg";
-import "./App.css"
+import "./App.css";
 
-export const App = () => <img src={svgUrl} width={200}/>
+export const App = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <img src={svgUrl} width={200} />
+    </>
+  );
+};
